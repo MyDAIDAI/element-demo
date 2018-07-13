fb43408c50d3f901fe747efce48f6270dc12dda7
daidai <571143755@qq.com>
2018-07-13 17:21:38 +0800

    add radio-button

---
+++ packages/radio-button/index.js
@@ -0,0 +1,6 @@
+import RadioButton from '../radio/src/radio-button.vue'
+
+RadioButton.install = function (Vue) {
+  Vue.component(RadioButton.name, RadioButton)
+}
+export default RadioButton
---
+++ packages/radio/src/radio-button.vue
@@ -0,0 +1,16 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'radio-button',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+
+</style>
